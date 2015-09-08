b55a2af3c51ea6b527a5820463f6d21140d76009
Carlos Saraiva <carlos.saraiva@msn.com>
2015-09-08 05:41:43 -0300

    Primeiro commit.

---
+++ server/routes/spwho.js
@@ -0,0 +1,35 @@
+﻿// ReSharper disable once UnusedParameter
+var express = require('express');
+var router = express.Router();
+var sql = require('mssql');
+
+var config = {
+    user: 'Claudio',
+    password: 'afc@55216',
+    server: 'servagilus.dyndns.org',
+    database: 'dbDemo'
+}
+
+router.get('/', function (req, res) {
+    spwho(function(r) {
+        res.json(r);    
+    });
+});
+
+function spwho(callback) {
+    var connection = new sql.Connection(config, function (err) {
+        var request = new sql.Request(connection);
+// ReSharper disable once DeclarationHides UnusedParameter
+// ReSharper disable UnusedParameter
+        request.execute('sp_who3', function (err, recordsets, returnValue) {
+// ReSharper restore UnusedParameter
+            callback(recordsets);
+        });
+    });
+    
+    connection.on('error', function (err) {
+        console.log(err);
+    });
+}
+
+module.exports = router;
---
+++ who/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace who
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public static EventHandler<string> OnSpWho;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            var update = new DispatcherTimer();
+            update.Interval = new TimeSpan(0, 0, 0, 3, 0);
+            update.Tick += (o, s) => DownloadDataAsync();
+            OnSpWho += (o, j) => listBox.ItemsSource = JsonConvert.DeserializeObject(j);
+
+            update.Start();
+        }
+
+        private static async void DownloadDataAsync()
+        {
+            using (var client = new HttpClient())
+            using (var response = await client.GetAsync("http://localhost:3000/spwho"))
+            using (var content = response.Content)
+            {
+                var result = await content.ReadAsStringAsync();
+                OnSpWho?.Invoke(null, result);
+            }
+        }
+    }
+}
